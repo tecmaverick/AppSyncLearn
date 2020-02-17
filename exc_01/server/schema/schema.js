7293b86f41151c218ecf6e5736c30d5c14b16b7d
Peter <abrpeter@acbc32c3e6d9.ant.amazon.com>
2020-02-17 17:55:38 +1100

    added test data; removed node_modules

--- exc_01/server/schema/schema.js
+++ exc_01/server/schema/schema.js
@@ -1,4 +1,14 @@
 const graphql = require("graphql");
+var _ = require("lodash")
+
+const usersData = [
+    {id:'1',name:'alpha',age:11},
+    {id:'2',name:'beta',age:12},
+    {id:'3',name:'gamma',age:13},
+    {id:'4',name:'tango',age:14},
+    {id:'5',name:'iota',age:15},
+    {id:'6',name:'zota',age:16}
+];
 
 const {
     GraphQLObjectType,
@@ -26,7 +36,7 @@ const RootQuery = new GraphQLObjectType({
             type: UserType,
             args: {id: { type:GraphQLString}},
             resolve(parent, args){
-                return {}
+                return _.find(usersData,{id: args.id})
             }
         }
 }})
